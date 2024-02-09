212d5f3e6509c0d735adcbab603c0f554d7347ee
VadimBaraa <158030729+VadimBaraa@users.noreply.github.com>
2024-02-09 19:30:00 +0300

    Add files via upload

---
+++ Modul_3.2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+
+        string MyName;
+
+        MyName = "Vadim";
+
+        Console.WriteLine(MyName);
+
+        Console.WriteLine("\t Привет Мир");
+        Console.WriteLine("\t Мне 27 лет");
+        Console.WriteLine("\t My name is \n {0}", MyName);
+        Console.WriteLine("\u0040"); //вывод символа @
+        Console.WriteLine("\u0023"); //вывод символа #
+
+        Console.ReadKey();
+    }
+}
---
+++ Modul_3.3.cs
@@ -0,0 +1,32 @@
+﻿//Работа с разными типами данных
+using System;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+
+        string MyName = "Jane";
+        byte MyAge = 27;
+        bool HaveIApet = true;
+        double MyShoeSize = 37.5;
+
+        Console.WriteLine("My name is " + MyName);
+        Console.WriteLine("MyAge " + MyAge);
+        Console.WriteLine("Do I have a pet? " + HaveIApet);
+        Console.WriteLine("My shoe size is " + MyShoeSize);
+
+    }
+}
+
+//Возможность узнать минимальные и максимальные значения типа без их запоминания
+using System;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("IntMin {0} ", int.MinValue);
+        Console.WriteLine("IntMax {0} ", int.MaxValue);
+    }
+}
